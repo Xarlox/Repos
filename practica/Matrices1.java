338121845c33523f31bd89eb705ba52a63ae1d57
Xarlox <carfran1925@hotmail.com>
2013-07-15 17:47:28 -0500

    Metodos
    Matrices

---
+++ practica/Matrices1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Matrices1 {
+
+    public static void main(String[] args) {
+        
+
+        Matrices1 c1 = new Matrices1();
+//        c1.totalCuentas++;
+//        System.out.println("Total cuentas: " + c1.totalCuentas);
+//        Matrices1 c2 = new Matrices1();
+//        c2.totalCuentas++;
+//        System.out.println("Total cuentas: " + c2.totalCuentas);
+// Acceso a traves de la clase:
+//        Matrices1.totalCuentas++;
+//        System.out.println("Total cuentas: " + Matrices1.totalCuentas);
+// Resto de sentencias . . .
+    }
+   public void totalCuentas(){
+    
+}
+}
